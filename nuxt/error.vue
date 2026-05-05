5c9db7c6caad838199c10e71bfd3b5bf03e4aabe
marcos-sco <marcos_sco@outlook.com>
2026-05-05 18:29:22 -0300

    feat: add error handling component with redirect functionality

---
+++ nuxt/error.vue
@@ -0,0 +1,24 @@
+<script setup>
+  /*import type { NuxtError } from "#app";
+  const props = defineProps({
+    error: Object as () => NuxtError,
+  });*/
+  const error = useError();
+
+  function handleRedirect() {
+    return navigateTo("/");
+  }
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Error: {{ error?.statusCode }}</h1>
+    <p>
+      {{ error?.statusMessage || "An error occurred" }}
+    </p>
+
+    <button @click="handleRedirect" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 my-4 px-4 rounded">
+      Go Home
+    </button>
+  </div>
+</template>
