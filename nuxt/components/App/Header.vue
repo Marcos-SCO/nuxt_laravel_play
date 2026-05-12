d2627ae487c5f4549f5ef2e233e6e335268bd18b
marcos-sco <marcos_sco@outlook.com>
2026-05-12 00:15:57 -0300

    feat: add informational top bar with close functionality

--- nuxt/components/App/Header.vue
+++ nuxt/components/App/Header.vue
@@ -16,10 +16,28 @@
       to: "/login",
     },
   ];
+
+  const topBarCookie = useCookie("showTopBar", {
+    maxAge: 60, // 1 minute
+  });
+  
+  const showTopBar = computed(() => {
+    return topBarCookie.value !== 'hidden';
+  });
+
+  const closeTopBar = () => {
+    topBarCookie.value = 'hidden';
+  };
+
 </script>
 
 <template>
   <header>
+    <UContainer v-if="showTopBar" class="bg-green-500 text-white text-center flex h-14 items-center justify-center">
+      <p class="text-sm font-medium text-center">This is an informational top bar message.</p>
+
+      <UButton @click="closeTopBar" icon="i-lucide-x" color="white" class="cursor-pointer" variant="ghost" square />
+    </UContainer>
     <UNavigationMenu :items="items" />
   </header>
 </template>
