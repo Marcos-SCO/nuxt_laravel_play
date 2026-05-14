2cf696c2ab1fd8657a3bd79519c6f072ad15e628
marcos-sco <marcos_sco@outlook.com>
2026-05-13 21:12:12 -0300

    feat: create Post model, factory, and migration for posts table

---
+++ laravel/database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(3);
+        return [
+            'title' => $title,
+            'slug' => str()->slug($title),
+            'user_id' => User::pluck('id')->random(),
+            'content' => $this->faker->paragraph(),
+            'thumb' => 'https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        ];
+    }
+}
